989a3901b8f00a76ad4cdf5536cf031121860dab
CalebManicke <134432959+CalebManicke@users.noreply.github.com>
2024-06-23 19:38:16 -0400

    Update ExtraSpacePNG.py

--- ImageProcessing/ExtraSpacePNG.py
+++ ImageProcessing/ExtraSpacePNG.py
@@ -4,7 +4,7 @@ import re
 import torch
 
 
-''' Given a directory of extracted bubbles, creates another directory of said bubbles pasted on pages 
+''' main(): Given a directory of extracted bubbles, creates another directory of said bubbles pasted on pages 
 
 1000 bubbles will produce 11 pages, first 10 pages have 96 bubbles each
 
@@ -126,82 +126,3 @@ def main(fileName, bubble_directory, misclassificationLabels, saveDir):
     print("Num labels:" + str(len(labels)))
     # Save labels in .torch file
     torch.save(labels, os.path.join(saveDir, fileName + '.torch'))
-
-
-if __name__ == '__main__':
-    
-    #fileNames = ['SVMValidation_Images', "SimpleCNNValidation_Images", "ResNet20Validation_Images", "DenseNetValidation_Images"]
-    #fileNames = ['Validation_BalCombined_SVM_Images']
-    #fileNames = ['Bubbles', 'Non_Bubbles']
-    modelNames = ['TWINS'] # ['SVM', 'SimpleCNN', 'ResNet20', 'DenseNet']
-    '''
-    for modelName in modelNames:
-        fileName = modelName + "_Validation"
-        #bubble_directory = "/home/caleb/VoterWork/ImageProcessing/Variable_Epsilon_Denoiser_Embedded_PrePrint_Bubbles/" + modelName + "Validation_Images"
-        #bubble_directory = '/home/caleb/VoterWork/TWINS/No_Denoiser_Embedded_TWINS_APGD_PrePrint_Bubbles/TWINS_Validation_Results'
-        bubble_directory = '/home/caleb/VoterWork/TWINS/All_Attacks_TWINS/' + modelName + "_Validation_Results"
-        misclassificationLabels = False
-
-        # Create the Val directory if it doesn't exist
-        saveDir = "/home/caleb/VoterWork/TWINS/All_Attacks_TWINS_PrePrint_Pages/" + modelName + "_Validation"
-        os.makedirs(saveDir, exist_ok=True)
-        #os.makedirs(saveDir + "_NonBubbles", exist_ok=True)
-
-        # Generate pages
-        #main(fileName, bubble_directory + "_Bubbles", misclassificationLabels, saveDir + "_Bubbles")
-        #main(fileName, bubble_directory + "_NonBubbles", misclassificationLabels, saveDir + "_NonBubbles")
-        main(fileName, bubble_directory, misclassificationLabels, saveDir)
-    '''
-    
-    '''
-    #fileNames = ["0.062_Adversarial_BalCombined_" + modelAttackStr + "_Images" for modelAttackStr in ["SimpleCNN_FGSM", "SimpleCNN_PGD", "SimpleCNN_PGD", "ResNet20_FGSM", "ResNet20_PGD", "ResNet20_PGD", "DenseNet_FGSM", "DenseNet_PGD", "DenseNet_PGD", "SimpleCNN_APGD", "ResNet20_APGD", "DenseNet_APGD"]]
-    #attackNames = ['FGSM', 'PGD', 'APGD']
-    attackNames = ['APGD']
-    #modelNames = ['SVM', 'SimpleCNN', 'ResNet20', 'DenseNet']
-    epsilonList = [0.0155, 0.031, 0.062, 0.124, 0.248]
-    for modelName in modelNames:
-        for attackName in attackNames:
-            for epsilon in epsilonList:
-                if (modelName == 'SVM' and attackName == 'FGSM') or (modelName != 'SVM'):
-                    fileName = modelName + "_" + attackName + "_" + str(epsilon) + "_Results"
-                    #bubble_directory = "/home/caleb/VoterWork/Models/No_Denoiser_Embedded_TWINS_APGD_PrePrint_Bubbles/" + modelName + "_APGD_" + str(epsilon) + "_Results"  #ImageProcessing/0.062_Adv_Denoiser_Embedded_PositiveGradient/" + fileName
-                    bubble_directory = "/home/caleb/VoterWork/TWINS/Denoiser_Embedded_Variable_Epsilon_APGD_TWINS/" + modelName + "_" + attackName + "_" + str(epsilon) + "_Results"
-                    misclassificationLabels = True
-
-                    # Create the Val directory if it doesn't exist
-                    saveDir = "/home/caleb/VoterWork/TWINS/Denoiser_Embedded_Variable_Epsilon_APGD_TWINS_PrePrint_Pages/" + modelName + "_" + attackName + "_" + str(epsilon)
-                    os.makedirs(saveDir, exist_ok=True)
-                    #os.makedirs(saveDir + "_No_Bubbles", exist_ok=True)
-
-                    # Generate pages
-                    main(fileName, bubble_directory, misclassificationLabels, saveDir)
-                    #main(fileName, bubble_directory + "_No_Bubbles_Results", misclassificationLabels, saveDir + "_No_Bubbles")
-    '''
-
-    '''
-    fileName = 'Denoiser_Training_Examples'
-    bubble_directory = '/home/caleb/VoterWork/Main/' + fileName
-    misclassificationLabels = False
-    saveDir = '/home/caleb/VoterWork/ImageProcessing/Pre_Print_Denoiser_Training_Example_Pages'
-    os.makedirs(saveDir, exist_ok = True)
-    main(fileName, bubble_directory, misclassificationLabels, saveDir)
-    '''
-
-    # Hyperparameters
-    epsilon_values = [0.01568, 0.03137, 0.06274, 0.12549, 0.25098, 1.0]
-    model_names = ['TWINS-B', 'TWINS-C'] # ['SVM-B', 'SVM-C', 'SimpleCNN-B', 'SimpleCNN-C', 'ResNet-20-B', 'ResNet-20-C']
-    base_bubble_dir = os.getcwd() + '//TWINS40x50//PrePrint_Bubbles//'
-    base_save_dir = os.getcwd() + '//TWINS40x50//PrePrint_Bubble_Sheets//'
-
-    for epsilon in epsilon_values:
-        for model in model_names:
-            # Retrieve bubble directory
-            fileName = model 
-            bubble_directory = base_bubble_dir + model + "//" + str(epsilon) 
-            misclassificationLabels = False
-
-            # Create the Val directory if it doesn't exist
-            saveDir = base_save_dir + model + "//" + str(epsilon) 
-            os.makedirs(saveDir, exist_ok=True)
-
-            main(fileName, bubble_directory, misclassificationLabels, saveDir)
